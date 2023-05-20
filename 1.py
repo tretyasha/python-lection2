a6a3051d8fe43cabad68cda5ab736a78711b55af
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-05-20 23:25:48 +0300

    done

---
+++ 1.py
@@ -0,0 +1,24 @@
+# list_1=[]
+# list_1=list()
+# list_1=[1,2,3,8]
+# # print(*list_1)
+
+# list_1=[]
+# list_1=list()
+# list_1=[1,2,3,8]
+# # for i in list_1:
+# #     print(i)
+
+# также можем узнать размер списка
+# list_1=[]
+# list_1=list()
+# list_1=[1,2,3,8]
+
+# print(len(list_1))
+
+# list_1=[]
+# list_1=list()
+# list_1=[1,2,3,8]
+
+# print(list_1[-2])
+
---
+++ 2.py
@@ -0,0 +1,14 @@
+# # научимся добавлять
+
+# list_1=[1,5]
+# print(list_1)
+# list_1.append(8)
+# print(list_1) #чтобы отслеживать наш результат
+# list_1.append(85)
+# print(list_1)
+
+# list_1=[]
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1)
